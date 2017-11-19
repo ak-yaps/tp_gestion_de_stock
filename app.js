8b6497e72648af2d277c68f839f56d362287a322
Ayaba Kumako <contact@ayaba-kumako.com>
2017-11-19 15:41:20 +0100

    fix(all)
    Modification du JS, HTML et CSS

--- app.js
+++ app.js
@@ -1,123 +1,83 @@
-var tbl = [];
+var tpStock = (function() {
+    var tbl, ref, nom, prix, couleur, poids, btnSupp, btnValider;
 
-// fonction contructeur
-var Produit = function(info) {
-  this.reference = info.reference;
-  this.nom = info.nom;
-  this.prix = info.prix;
-  this.couleur = info.couleur;
-  this.poids = info.poids;
-};
-
-// fonction pour créer un nouveau produit
-var saisirProduit = function() {
-
-  var reference = 0;
-  var nom = document.getElementById('nom');
-  var prix = document.getElementById('prix');
-  var couleur = document.getElementById('couleur');
-  var poids = document.getElementById('poids');
-
-
-  var p = new Produit({
-    reference: 'Ref ' + reference++,
-    nom: nom.value,
-    prix: prix.value,
-    couleur: couleur.value,
-    poids: poids.value
-  });
-
-  tbl.push(p);
-
-  // créer un tableau
-  for (var i = 0; i < tbl.length; i++) {
-
-    var nouvelleLigne = document.querySelector("tbody").insertRow(-1);
-
-    function verifNom() {
-      if (nom.value === "") {
-        return false;
-      } else {
-        var colonne0 = nouvelleLigne.insertCell(0);
-        colonne0.innerHTML += tbl[i].reference;
-
-        var colonne1 = nouvelleLigne.insertCell(1);
-        colonne1.innerHTML += tbl[i].nom;
-      }
-    };
-
-    function verifPrix(e) {
-      if (isNaN(prix.value) === false && prix.value !== "" || typeof(prix.value) === 'number') {
-        var colonne2 = nouvelleLigne.insertCell(2);
-        colonne2.innerHTML += tbl[i].prix + ' &euro;';
-      } else {
-        return false;
-      };
-    };
-
-    function verifCouleur() {
-      if (couleur.value === "") {
-        return false;
-      } else {
-        var colonne3 = nouvelleLigne.insertCell(3);
-        colonne3.innerHTML += tbl[i].couleur;
-      }
-    };
-
-    function verifPoids() {
-      if (isNaN(poids.value) === false && poids.value !== "" || typeof(poids.value) === 'number') {
-        var colonne4 = nouvelleLigne.insertCell(4);
-        colonne4.innerHTML += tbl[i].poids;
-
-        var colonne5 = nouvelleLigne.insertCell(5);
-        colonne5.innerHTML += '<button class="btnSupp">Supprimer</button>';
-      } else {
-        return false;
-      }
-    };
-
-    function verifForm() {
-      var nomOk = verifNom();
-      var prixOk = verifPrix();
-      var couleurOk = verifCouleur();
-      var poidsOk = verifPoids();
-
-      if (nomOk && prixOk && couleurOk && poidsOk) {
-          return true;
-      } else {
-        alert("Veuillez remplir correctement tous les champs");
-        tbl.splice(0, 1);
-        return false;
-      }
+    function getId(id) {
+      return document.getElementById(id);
     }
 
-    verifForm();
+    ref = 0;
+    nom = getId('nom');
+    prix = getId('prix');
+    couleur = getId('couleur');
+    poids = getId('poids');
+    btnValider = getId('btnValider');
 
-    // fonction Supprimer
-    var btnSupp = document.querySelectorAll('.btnSupp');
+    function getNewProduct() {
+      btnValider.addEventListener('click', function() {
 
-    for (var k = btnSupp.length - 1; k < btnSupp.length; k++) {
-      btnSupp[k].addEventListener("click", function(e) {
-        var target = e.target || e.srcElement;
-        suppRow = target.parentNode.parentNode.rowIndex;
-        document.getElementById('table').deleteRow(suppRow);
+        var Produit = function() {
+          this.ref = ref,
+            this.nom = nom.value,
+            this.prix = prix.value,
+            this.couleur = couleur.value,
+            this.poids = poids.value
+        }
+
+        var nvoProduit = new Produit({
+          ref: ref++,
+          nom: nom,
+          prix: prix,
+          couleur: couleur,
+          poids: poids
+        })
+
+        if ((isNaN(nvoProduit.prix && nvoProduit.poids) === false) && ((nvoProduit.nom && nvoProduit.prix && nvoProduit.couleur && nvoProduit.poids) !== "")) {
+          tbl.push(nvoProduit);
+        } else {
+          alert('Veuillez remplir correctement les champs');
+          ref--;
+        }
+
+        for (var i = tbl.length - 1; i < tbl.length; i++) {
+          var newRow = document.querySelector("tbody").insertRow(-1);
+
+          if ((isNaN(nvoProduit.prix && nvoProduit.poids) === false) && ((nvoProduit.nom && nvoProduit.prix && nvoProduit.couleur && nvoProduit.poids) !== "")) {
+            var col0 = newRow.insertCell(0);
+            col0.innerHTML += 'ref ' + tbl[i].ref;
+          }
+          var col1 = newRow.insertCell(1);
+          col1.innerHTML += tbl[i].nom;
+          var col2 = newRow.insertCell(2);
+          col2.innerHTML += tbl[i].prix + '	&euro;';
+          var col3 = newRow.insertCell(3);
+          col3.innerHTML += tbl[i].couleur;
+          var col4 = newRow.insertCell(4);
+          col4.innerHTML += tbl[i].poids + ' g';
+          var col5 = newRow.insertCell(5);
+          col5.innerHTML += '<button class="btnSupp">Supprimer</button>';
+
+          var btnSupp = document.querySelectorAll('.btnSupp');
+          for (var k = btnSupp.length - 1; k < btnSupp.length; k++) {
+            btnSupp[k].addEventListener("click", function(e) {
+              var target = e.target || e.srcElement;
+              suppRow = target.parentNode.parentNode.rowIndex;
+              document.getElementById('table').deleteRow(suppRow);
+            })
+          }
+          tbl.splice(0, 1);
+        }
+
+        var input = document.querySelectorAll('input');
+        for (var j = 0; j < input.length; j++) {
+          input[j].value = input[j].defaultValue;
+        };
       })
+    };
+
+    window.onload = function() {
+      tbl = [];
+      getNewProduct();
+      console.log(tbl);
     }
-  };
-
-  // effacer les champs à chaque saisie
-  var input = document.querySelectorAll('input');
-  for (var j = 0; j < input.length; j++) {
-    input[j].value = input[j].defaultValue;
-  };
-
-  // supprime l'élément précedent dans le tableau JS
-  tbl.splice(0, 1);
-
-};
-
-document.getElementById("btnValider").onclick = function() {
-  saisirProduit();
-};
-
-// saisirProduit();
+  }
+  ());
